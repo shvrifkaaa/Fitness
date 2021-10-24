a1504f77f06ad5895df9dbdeaae97375e3992279
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-10-24 18:09:02 +0600

    3th commit. Created 2nd post. fixing post_text2

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let post1 = document.querySelector ('.post1'); let post1_text = document.querySelector ('#post1_text') 
+
+
+
+post1_text.addEventListener('mousemove', function(){
+    post1_text.setAttribute(' '   )
+
+})
