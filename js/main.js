30e1fb7ee8505c17c7813e7f115cab13cdae17e3
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-10-24 19:18:15 +0600

    3.1 commit. Fixed 2nd post.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let post1 = document.querySelector ('.post1'); let post1_text = document.querySelector ('#post1_text') 
-
-
-
-post1_text.addEventListener('mousemove', function(){
-    post1_text.setAttribute(' '   )
-
-})
