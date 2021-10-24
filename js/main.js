55f9d413b0bf1120ee9a32c4273b2db3412f1a3e
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-10-24 22:54:24 +0600

    4th commit. fixed 2nd post. debugged 1st post. fixed header.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function scrollTo(element){
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+    })
+}
+
+var header_about = document.querySelector('#btn_about');
+var post2 = document.querySelector('.post2')
+
+header_about.addEventListener('click', () => {
+    scrollTo(post2);
+})
+
+var end = document.querySelector('#text3');
+
+end.waypoint(function () {
+    console.log('hello world');
+})
