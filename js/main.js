12e3849381bb8fe4238e8b05bdfc1c982c179573
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-11-09 00:24:18 +0600

    6.2 commit. Ended Invite_block.

--- js/main.js
+++ js/main.js
@@ -7,11 +7,28 @@ function scrollTo(element){
 }
 
 var header_about = document.querySelector('#btn_about');
-var post2 = document.querySelector('.post2')
-
+var post2 = document.querySelector('.post2');
 header_about.addEventListener('click', () => {
     scrollTo(post2);
+    console.log('df')
 })
+
+var header_contact = document.querySelector('#btn_contact');
+var invite = document.querySelector('#invite');
+
+header_contact.addEventListener('click', () =>{
+    scrollTo(invite)
+    console.log('dfhjd')
+})
+
+
+
+
+
+
+
+
+
 
 // var $end = document.querySelector('#text3');
 
