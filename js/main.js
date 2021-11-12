1683d25762ebf27c10f72ec7d02914e4544dc649
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-11-12 23:12:00 +0600

    7th commit. Fixed All start page, fixed div Challenges, invite_block, footer.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-function scrollTo(element){
+$(document).ready(function () { // Ждём загрузки страницы // АНИМАЦИЯ ЦВЕТА ШАПКИ
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            anim();
+            console.log('hello')
+        }
+    });
+});
+
+function anim() {
+    $('#header').css(anim_style);
+}
+var anim_style = {
+    "background-color": "rgb(181,175,239)",
+    "transition": "0.5s",
+    "box-shadow": "0 0 20px 0 rgba(115,98,223,1)"
+}                                               // АНИМАЦИЯ ЦВЕТА ШАПКИ ПРИ СКРОЛЛЕ
+
+
+
+
+function scrollTo(element){             // ПЛАВНЫЙ СКРОЛЛ ДО ЭЛЕМЕНТОВ ПРИ НАЖАТИИ
     window.scroll({
         left: 0,
         top: element.offsetTop,
@@ -14,24 +35,9 @@ header_about.addEventListener('click', () => {
 })
 
 var header_contact = document.querySelector('#btn_contact');
-var invite = document.querySelector('#invite');
+var invite = document.querySelector('.invite');
 
 header_contact.addEventListener('click', () =>{
     scrollTo(invite)
     console.log('dfhjd')
-})
-
-
-
-
-
-
-
-
-
-
-// var $end = document.querySelector('#text3');
-
-// $.waypoint(function () {
-//     console.log('hello world');
-// })
+});
