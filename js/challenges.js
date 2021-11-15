c7172348bce0fcf4e530b7b2169e55aca60bd2ba
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-11-15 23:35:51 +0600

    8th com. Created index Challenges(Created post1)

---
+++ js/challenges.js
@@ -0,0 +1,17 @@
+$(document).ready(function () { // Ждём загрузки страницы // АНИМАЦИЯ ЦВЕТА ШАПКИ
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            anim();
+            console.log('hello')
+        }
+    });
+});
+
+function anim() {
+    $('#header').css(anim_style);
+}
+var anim_style = {
+    "background-color": "rgb(181,175,239)",
+    "transition": "0.5s",
+    "box-shadow": "0 0 20px 0 rgba(115,98,223,1)"
+} // АНИМАЦИЯ ЦВЕТА ШАПКИ ПРИ СКРОЛЛЕ
