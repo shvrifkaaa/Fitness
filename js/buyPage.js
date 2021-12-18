02e850a1134f39e7e6cd56fc1813378cafda7d5e
Shvrif Kalandarov <kalandarov16.kg@gmail.com>
2021-12-18 16:33:43 +0600

    13th commit. Fixed all pages. Maked adaptive page (main page)

--- js/buyPage.js
+++ js/buyPage.js
@@ -14,3 +14,15 @@ var anim_style = {
     "transition": "0.5s",
     "box-shadow": "0 0 20px 0 rgba(115,98,223,1)"
 } // АНИМАЦИЯ ЦВЕТА ШАПКИ ПРИ СКРОЛЛЕ
+
+function scrollTo(element) { // ПЛАВНЫЙ СКРОЛЛ ДО ЭЛЕМЕНТОВ ПРИ НАЖАТИИ
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+    })
+}
+var btn_end = document.querySelector ('#btn_end')
+var join_btn = document.querySelector('#btn_scroll').addEventListener('click', () => {
+    scrollTo(btn_end);
+})
--- js/main.js
+++ js/main.js
@@ -27,30 +27,48 @@ function scrollTo(element) { // ПЛАВНЫЙ СКРОЛЛ ДО ЭЛЕМЕНТ�
 }
 var footer_about_btn = document.querySelector('#footer_left_about_btn');
 var footer_home_btn = document.querySelector('#footer_left_home_btn');
-var header_about = document.querySelector('#btn_about');
+var header_about = document.querySelector('.btn_about');
 var post2 = document.querySelector('.post2');
-var header_contact = document.querySelector('#btn_contact');
+var header_contact = document.querySelector('.btn_contact');
 var invite = document.querySelector('.invite');
 var post1 = document = document.querySelector('.post1');
 var footer_contact_btn = document.querySelector('#footer_left_contact_btn');
+var burger_trainers = document.querySelector('.btn_trainers')
+var trainers = document.querySelector ('.trainers')
+var burger_invite = document.querySelector ('.btn_invite')
+var burger_contact = document.querySelector('.btn_contact')
+var footer = document.querySelector('footer')
+burger_invite.addEventListener('click', () => {
+    scrollTo(invite)
+    console.log('scrolled to invite')
+})
+burger_contact.addEventListener('click', ()=> { 
+    scrollTo(footer)
+    console.log('scrolled to footer')
+})
+
+burger_trainers.addEventListener('click', () => { 
+    scrollTo(trainers)
+    console.log('scrolled to trainers')
+})
 
 header_about.addEventListener('click', () => {
     scrollTo(post2);
-    console.log('df')
+    console.log('scrolled to post2')
 })
-
 header_contact.addEventListener('click', () => {
     scrollTo(invite)
-    console.log('dfhjd')
+    console.log('scrolled to invite')
 });
-
 footer_home_btn.addEventListener('click', () => {
     scrollTo(post1);
-    console.log('footer_home')
+    console.log('scrolled to post1')
 })
 footer_about_btn.addEventListener('click', () => {
     scrollTo(post2);
+    console.log('scrolled to post2')
 })
 footer_contact_btn.addEventListener('click', () => {
     scrollTo(invite)
+    console.log('scrolled to invite')
 })
